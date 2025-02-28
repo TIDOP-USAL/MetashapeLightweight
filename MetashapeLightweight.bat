57c870571a819ccb4969a841a62af58cb02032fa
DHL <david.hernandez@uclm.es>
2025-02-28 16:34:13 +0100

    Primera versión funcionando lanzando desde MetashapeLightweight.bat

---
+++ MetashapeLightweight.bat
@@ -0,0 +1,7 @@
+echo off
+SETLOCAL
+set ANACONDA_PATH=C:/miniconda
+set ANACONDA_ENV_NAME=metashape_2_2_0
+call %ANACONDA_PATH%/Scripts/activate.bat %ANACONDA_ENV_NAME%
+python  main_gui.py
+ENDLOCAL
