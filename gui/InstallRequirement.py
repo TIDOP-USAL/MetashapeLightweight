04e7eb37a4a29f5f0de36a6d4f8bc861648a5fac
DHL <david.hernandez@uclm.es>
2025-03-09 12:32:53 +0100

    .

--- gui/InstallRequirement.py
+++ gui/InstallRequirement.py
@@ -8,21 +8,21 @@ class InstallRequirement:
 		self.__json_content_by_propierty = {}
 		self.__json_content_by_propierty['text'] = {'spanish': 'Requisitos de instalacion', 'english': 'Install requirements'}
 		self.__text = 'Requisitos de instalacion'
-		self.__json_content_by_propierty['CondaPath'] = {'text': {'spanish': 'Carpeta de instalacion de conda/miniconda', 'english': 'Conda/miniconda installation folder'}, 'definition': {'spanish': 'Carpeta de instalacion de conda/miniconda', 'english': 'Conda/miniconda installation folder'}, 'type': 'folder', 'default': ''}
-		self.__text_by_propierty['CondaPath'] = 'Carpeta de instalacion de conda/miniconda'
-		self.__widget_by_propierty['CondaPath'] = None
-		self.__CondaPath = ""
-		self.__CondaPath_value = ""
-		self.__json_content_by_propierty['Env'] = {'text': {'spanish': 'Nombre del entorno conda', 'english': 'Conda environment name'}, 'definition': {'spanish': 'Nombre del entorno conda', 'english': 'Conda environment name'}, 'type': 'string', 'default': ''}
-		self.__text_by_propierty['Env'] = 'Nombre del entorno conda'
-		self.__widget_by_propierty['Env'] = None
-		self.__Env = ""
-		self.__Env_value = ""
 		self.__json_content_by_propierty['Geoid'] = {'text': {'spanish': 'Carpeta con los modelos del geoide', 'english': 'Geoids folder'}, 'definition': {'spanish': 'Carpeta con los modelos del geoide', 'english': 'Geoids folder'}, 'type': 'folder', 'default': ''}
 		self.__text_by_propierty['Geoid'] = 'Carpeta con los modelos del geoide'
 		self.__widget_by_propierty['Geoid'] = None
 		self.__Geoid = ""
 		self.__Geoid_value = ""
+		self.__json_content_by_propierty['SelectedGPUs'] = {'text': {'spanish': 'GPUs activadas', 'english': 'Activated GPUs'}, 'definition': {'spanish': 'Seleccion de GPUs activadas para el proceso', 'english': 'Selection of activated GPUs for processing'}, 'type': 'string', 'len': 1000, 'default': ''}
+		self.__text_by_propierty['SelectedGPUs'] = 'GPUs activadas'
+		self.__widget_by_propierty['SelectedGPUs'] = None
+		self.__SelectedGPUs = ""
+		self.__SelectedGPUs_value = ""
+		self.__json_content_by_propierty['UseCPU'] = {'text': {'spanish': 'Utilizar la CPU', 'english': 'Use CPU'}, 'definition': {'spanish': 'Utilizar la CPU al realizar un procesamiento acelerado por GPU', 'english': 'Use CPU when performing GPU accelerated processing'}, 'type': 'boolean', 'default': 'False'}
+		self.__text_by_propierty['UseCPU'] = 'Utilizar la CPU'
+		self.__widget_by_propierty['UseCPU'] = None
+		self.__UseCPU = False
+		self.__UseCPU_value = False
 		self.__widget = None
 
 	def get_propierty_json_content(self, value):
@@ -33,9 +33,9 @@ class InstallRequirement:
 
 	def get_values_as_dictionary(self):
 		values = {}
-		values['CondaPath'] = self.__CondaPath_value
-		values['Env'] = self.__Env_value
 		values['Geoid'] = self.__Geoid_value
+		values['SelectedGPUs'] = self.__SelectedGPUs_value
+		values['UseCPU'] = self.__UseCPU_value
 		return values
 
 	def get_text(self):
@@ -49,48 +49,6 @@ class InstallRequirement:
 		if value in self.__widget_by_propierty:
 			widget_propierty = self.__widget_by_propierty[value]
 		return widget_propierty
-
-	@property
-	def CondaPath(self):
-		return self.__CondaPath
-
-	@CondaPath.setter
-	def CondaPath(self, value: 'widget:file, type:folder, toolTip:Carpeta de instalacion de conda/miniconda'):
-		self.__CondaPath = value
-
-	def set_CondaPath_value(self):
-		propierty_CondaPath_widget = self.__widget_by_propierty['CondaPath'] 
-		if isinstance(propierty_CondaPath_widget, QSpinBox):
-			self.__CondaPath_value = propierty_CondaPath_widget.value()
-		elif isinstance(propierty_CondaPath_widget, QDoubleSpinBox):
-			self.__CondaPath_value = propierty_CondaPath_widget.value()
-		elif isinstance(propierty_CondaPath_widget, QComboBox):
-			self.__CondaPath_value = propierty_CondaPath_widget.currentText()
-		elif isinstance(propierty_CondaPath_widget, QLineEdit):
-			self.__CondaPath_value = propierty_CondaPath_widget.text()
-		elif isinstance(propierty_CondaPath_widget, QCheckBox):
-			self.__CondaPath_value = propierty_CondaPath_widget.isChecked()
-
-	@property
-	def Env(self):
-		return self.__Env
-
-	@Env.setter
-	def Env(self, value: 'widget:QLineEdit, toolTip:Nombre del entorno conda'):
-		self.__Env = value
-
-	def set_Env_value(self):
-		propierty_Env_widget = self.__widget_by_propierty['Env'] 
-		if isinstance(propierty_Env_widget, QSpinBox):
-			self.__Env_value = propierty_Env_widget.value()
-		elif isinstance(propierty_Env_widget, QDoubleSpinBox):
-			self.__Env_value = propierty_Env_widget.value()
-		elif isinstance(propierty_Env_widget, QComboBox):
-			self.__Env_value = propierty_Env_widget.currentText()
-		elif isinstance(propierty_Env_widget, QLineEdit):
-			self.__Env_value = propierty_Env_widget.text()
-		elif isinstance(propierty_Env_widget, QCheckBox):
-			self.__Env_value = propierty_Env_widget.isChecked()
 
 	@property
 	def Geoid(self):
@@ -112,6 +70,48 @@ class InstallRequirement:
 			self.__Geoid_value = propierty_Geoid_widget.text()
 		elif isinstance(propierty_Geoid_widget, QCheckBox):
 			self.__Geoid_value = propierty_Geoid_widget.isChecked()
+
+	@property
+	def SelectedGPUs(self):
+		return self.__SelectedGPUs
+
+	@SelectedGPUs.setter
+	def SelectedGPUs(self, value: 'widget:QLineEdit, toolTip:Seleccion de GPUs activadas para el proceso'):
+		self.__SelectedGPUs = value
+
+	def set_SelectedGPUs_value(self):
+		propierty_SelectedGPUs_widget = self.__widget_by_propierty['SelectedGPUs'] 
+		if isinstance(propierty_SelectedGPUs_widget, QSpinBox):
+			self.__SelectedGPUs_value = propierty_SelectedGPUs_widget.value()
+		elif isinstance(propierty_SelectedGPUs_widget, QDoubleSpinBox):
+			self.__SelectedGPUs_value = propierty_SelectedGPUs_widget.value()
+		elif isinstance(propierty_SelectedGPUs_widget, QComboBox):
+			self.__SelectedGPUs_value = propierty_SelectedGPUs_widget.currentText()
+		elif isinstance(propierty_SelectedGPUs_widget, QLineEdit):
+			self.__SelectedGPUs_value = propierty_SelectedGPUs_widget.text()
+		elif isinstance(propierty_SelectedGPUs_widget, QCheckBox):
+			self.__SelectedGPUs_value = propierty_SelectedGPUs_widget.isChecked()
+
+	@property
+	def UseCPU(self):
+		return self.__UseCPU
+
+	@UseCPU.setter
+	def UseCPU(self, value: 'widget:QCheckBox, toolTip:Utilizar la CPU al realizar un procesamiento acelerado por GPU'):
+		self.__UseCPU = value
+
+	def set_UseCPU_value(self):
+		propierty_UseCPU_widget = self.__widget_by_propierty['UseCPU'] 
+		if isinstance(propierty_UseCPU_widget, QSpinBox):
+			self.__UseCPU_value = propierty_UseCPU_widget.value()
+		elif isinstance(propierty_UseCPU_widget, QDoubleSpinBox):
+			self.__UseCPU_value = propierty_UseCPU_widget.value()
+		elif isinstance(propierty_UseCPU_widget, QComboBox):
+			self.__UseCPU_value = propierty_UseCPU_widget.currentText()
+		elif isinstance(propierty_UseCPU_widget, QLineEdit):
+			self.__UseCPU_value = propierty_UseCPU_widget.text()
+		elif isinstance(propierty_UseCPU_widget, QCheckBox):
+			self.__UseCPU_value = propierty_UseCPU_widget.isChecked()
 
 	def set_values_from_dictionary(self, values):
 		for value in values:
@@ -139,32 +139,6 @@ class InstallRequirement:
 
 	def set_widget(self, widget):
 		self.__widget = widget
-		propierty_CondaPath_widget = self.__widget.get_widget('CondaPath')
-		if isinstance(propierty_CondaPath_widget, QSpinBox):
-			propierty_CondaPath_widget.valueChanged.connect(self.set_CondaPath_value)
-		elif isinstance(propierty_CondaPath_widget, QDoubleSpinBox):
-			propierty_CondaPath_widget.valueChanged.connect(self.set_CondaPath_value)
-		elif isinstance(propierty_CondaPath_widget, QComboBox):
-			propierty_CondaPath_widget.currentIndexChanged.connect(self.set_CondaPath_value)
-		elif isinstance(propierty_CondaPath_widget, QLineEdit):
-			propierty_CondaPath_widget.editingFinished.connect(self.set_CondaPath_value)
-			propierty_CondaPath_widget.textChanged.connect(self.set_CondaPath_value)
-		elif isinstance(propierty_CondaPath_widget, QCheckBox):
-			propierty_CondaPath_widget.stateChanged.connect(self.set_CondaPath_value)
-		self.__widget_by_propierty['CondaPath'] = propierty_CondaPath_widget
-		propierty_Env_widget = self.__widget.get_widget('Env')
-		if isinstance(propierty_Env_widget, QSpinBox):
-			propierty_Env_widget.valueChanged.connect(self.set_Env_value)
-		elif isinstance(propierty_Env_widget, QDoubleSpinBox):
-			propierty_Env_widget.valueChanged.connect(self.set_Env_value)
-		elif isinstance(propierty_Env_widget, QComboBox):
-			propierty_Env_widget.currentIndexChanged.connect(self.set_Env_value)
-		elif isinstance(propierty_Env_widget, QLineEdit):
-			propierty_Env_widget.editingFinished.connect(self.set_Env_value)
-			propierty_Env_widget.textChanged.connect(self.set_Env_value)
-		elif isinstance(propierty_Env_widget, QCheckBox):
-			propierty_Env_widget.stateChanged.connect(self.set_Env_value)
-		self.__widget_by_propierty['Env'] = propierty_Env_widget
 		propierty_Geoid_widget = self.__widget.get_widget('Geoid')
 		if isinstance(propierty_Geoid_widget, QSpinBox):
 			propierty_Geoid_widget.valueChanged.connect(self.set_Geoid_value)
@@ -178,3 +152,29 @@ class InstallRequirement:
 		elif isinstance(propierty_Geoid_widget, QCheckBox):
 			propierty_Geoid_widget.stateChanged.connect(self.set_Geoid_value)
 		self.__widget_by_propierty['Geoid'] = propierty_Geoid_widget
+		propierty_SelectedGPUs_widget = self.__widget.get_widget('SelectedGPUs')
+		if isinstance(propierty_SelectedGPUs_widget, QSpinBox):
+			propierty_SelectedGPUs_widget.valueChanged.connect(self.set_SelectedGPUs_value)
+		elif isinstance(propierty_SelectedGPUs_widget, QDoubleSpinBox):
+			propierty_SelectedGPUs_widget.valueChanged.connect(self.set_SelectedGPUs_value)
+		elif isinstance(propierty_SelectedGPUs_widget, QComboBox):
+			propierty_SelectedGPUs_widget.currentIndexChanged.connect(self.set_SelectedGPUs_value)
+		elif isinstance(propierty_SelectedGPUs_widget, QLineEdit):
+			propierty_SelectedGPUs_widget.editingFinished.connect(self.set_SelectedGPUs_value)
+			propierty_SelectedGPUs_widget.textChanged.connect(self.set_SelectedGPUs_value)
+		elif isinstance(propierty_SelectedGPUs_widget, QCheckBox):
+			propierty_SelectedGPUs_widget.stateChanged.connect(self.set_SelectedGPUs_value)
+		self.__widget_by_propierty['SelectedGPUs'] = propierty_SelectedGPUs_widget
+		propierty_UseCPU_widget = self.__widget.get_widget('UseCPU')
+		if isinstance(propierty_UseCPU_widget, QSpinBox):
+			propierty_UseCPU_widget.valueChanged.connect(self.set_UseCPU_value)
+		elif isinstance(propierty_UseCPU_widget, QDoubleSpinBox):
+			propierty_UseCPU_widget.valueChanged.connect(self.set_UseCPU_value)
+		elif isinstance(propierty_UseCPU_widget, QComboBox):
+			propierty_UseCPU_widget.currentIndexChanged.connect(self.set_UseCPU_value)
+		elif isinstance(propierty_UseCPU_widget, QLineEdit):
+			propierty_UseCPU_widget.editingFinished.connect(self.set_UseCPU_value)
+			propierty_UseCPU_widget.textChanged.connect(self.set_UseCPU_value)
+		elif isinstance(propierty_UseCPU_widget, QCheckBox):
+			propierty_UseCPU_widget.stateChanged.connect(self.set_UseCPU_value)
+		self.__widget_by_propierty['UseCPU'] = propierty_UseCPU_widget
